44eade5843ff0434de725f7b2c14872ab48db0fb
Bruno1435 <152212891+Bruno1435@users.noreply.github.com>
2023-12-05 15:41:35 +0000

    corrigindo logica do arquivo JS

--- gerador de senha/script.js
+++ gerador de senha/script.js
@@ -6,12 +6,31 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!#*"
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderelement.value;
 
 slider.oninput = function(){
     sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderelement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+    
+} 
+
+function copyPassword(){
+    alert("Senha copiada com sucesso!");
+    navigator.clipboard.writeText(novaSenha);
 }
 
